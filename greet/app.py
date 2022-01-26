4ba2b95b781654d2b5507790d9d85d457c1a585c
= <stevenmtang1@gmail.com>
2022-01-26 12:14:46 -0800

    Completed greet

---
+++ greet/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+@app.get("/welcome")
+def welcome_page():
+    return "welcome"
+
+@app.get("/welcome/home")
+def welcome_home():
+    return "welcome home"
+
+@app.get("/welcome/back")
+def welcome_back():
+    return "welcome back"
