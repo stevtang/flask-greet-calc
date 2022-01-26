88f375f9026dcdb3f69864e34abd554b04c3f4dd
Brian Aston <astonb.dev@gmail.com>
2022-01-26 15:34:29 -0800

    Finished calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,20 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, div, mult, sub
+
+app = Flask(__name__)
+
+OPERATIONS = {"add": add, "sub": sub, "div": div, "mult": mult}
+
+"""Basic math operations."""
+
+
+@app.get("/math/<operation>")
+def show_operation_result(operation):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    math_operation = OPERATIONS.get(operation)
+
+    return str(math_operation(a, b))
+
